34dddf20a2d05de4f17a4a62c8fbfa5740d8337f
ZhengyuanWu <464720715@qq.com>
2025-10-11 15:41:42 +0800

    refactor: create py files for Money and Portfolio classes; add gitignore file

--- py/test_money.py
+++ py/test_money.py
@@ -1,15 +1,8 @@
 import unittest
-import functools
-import operator
+from money import Money
+from portfolio import Portfolio
 
 class TestMoney(unittest.TestCase):
-    def test_foo(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_multiplicationInDollar(self):
-        fiveDollars = Money(5, "USD")
-        tenDollars = Money(10, "USD")
-        self.assertEqual(tenDollars, fiveDollars.times(2))
 
     def test_multiplicationInEuros(self):
         tenEuros = Money(10, "EUR")
@@ -30,32 +23,6 @@ class TestMoney(unittest.TestCase):
         portfolio = Portfolio()
         portfolio.add(fiveDollars, tenDollars)
         self.assertEqual(fifteenDollars, portfolio.evaluate("USD"))
-
-class Money:
-    def __init__(self, amount, currency):
-        self.amount = amount
-        self.currency = currency
-
-    def __eq__(self, other):
-        return self.amount == other.amount and self.currency == other.currency
-
-    def times(self, multiplier):
-        return Money(self.amount * multiplier, self.currency)
-
-    def divide(self, divisor):
-        return Money(self.amount/divisor, self.currency)
-
-class Portfolio:
-    def __init__(self):
-        self.moneys = []
-
-    def add(self, *moneys):
-        self.moneys.extend(moneys)
-
-    def evaluate(self, currency):
-        total = functools.reduce(
-                operator.add, map(lambda m: m.amount, self.moneys), 0)
-        return Money(total, currency)
 
 if __name__ == '__main__':
     unittest.main()
