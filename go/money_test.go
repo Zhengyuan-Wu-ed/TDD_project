730cfd56b57996cdfc4f35073addbb525c8c4dc8
ZhengyuanWu <464720715@qq.com>
2025-10-11 10:36:06 +0800

    feat: division and multiplication features done

--- go/money_test.go
+++ go/money_test.go
@@ -4,20 +4,45 @@ import (
 	"testing"
 )
 
-func TestMultiplication(t *testing.T) {
-	fiver := Dollar{
-		amount: 5,
-	}
-	tenner := fiver.Times(2)
-	if tenner.amount != 10 {
-		t.Errorf("Expected 10, got: [%d]", tenner.amount)
+func assertEqual(t *testing.T, expected Money, actual Money) {
+	if expected != actual {
+		t.Errorf("Expected %+v Got %+v", expected, actual)
 	}
 }
 
-type Dollar struct {
-	amount int
+func TestMultiplicationInDollars(t *testing.T) {
+	originalMoney := Money{amount: 5, currency: "USD"}
+	actualResult := originalMoney.Times(2)
+	
+	expectedResult := Money{amount: 10, currency: "USD"}
+	assertEqual(t, expectedResult, actualResult)	
 }
 
-func (d Dollar) Times(multiplier int) Dollar {
-	return Dollar{amount: d.amount * multiplier}
+func TestMultiplicationInEuros(t *testing.T) {
+	originalMoney:= Money{amount: 10, currency: "EUR"}
+	actualResult := originalMoney.Times(2)
+
+	expectedResult := Money{amount: 20, currency: "EUR"}
+	assertEqual(t, expectedResult, actualResult)	
+}
+
+func TestDivision(t *testing.T) {
+	originalMoney := Money{amount: 4002, currency: "KRW"}
+	actualMoneyAfterDivision := originalMoney.Divide(4)
+	
+	expectedMoneyAfterDivision := Money{amount: 1000.5, currency: "KRW"}
+	assertEqual(t, expectedMoneyAfterDivision, actualMoneyAfterDivision)	
+}
+
+type Money struct {
+	amount float64
+	currency string
+}
+
+func (m Money) Times(multiplier int) Money {
+	return Money{amount: m.amount*float64(multiplier), currency: m.currency}
+}
+
+func (m Money) Divide(divisor int) Money {
+	return Money{amount: m.amount/float64(divisor), currency: m.currency}
 }
--- py/test_money.py
+++ py/test_money.py
@@ -4,17 +4,35 @@ class TestMoney(unittest.TestCase):
     def test_foo(self):
         self.assertEqual('foo'.upper(), 'FOO')
 
-    def test_multiplication(self):
-        fiver = Dollar(5)
-        tenner = fiver.times(2)
-        self.assertEqual(10, tenner.amount)
+    def test_multiplicationInDollar(self):
+        fiveDollars = Money(5, "USD")
+        tenDollars = Money(10, "USD")
+        self.assertEqual(tenDollars, fiveDollars.times(2))
 
-class Dollar:
-    def __init__(self, amount):
+    def test_multiplicationInEuros(self):
+        tenEuros = Money(10, "EUR")
+        twentyEuros = Money(20, "EUR") 
+        self.assertEqual(twentyEuros, tenEuros.times(2))
+
+    def test_division(self):
+        originalMoney = Money(4002, "KRW")
+        actualMoneyAfterDivision = originalMoney.divide(4)
+        expectedMoneyAfterDivision = Money(1000.5, "KRW")
+        self.assertEqual(expectedMoneyAfterDivision, actualMoneyAfterDivision)
+
+class Money:
+    def __init__(self, amount, currency):
         self.amount = amount
+        self.currency = currency
+
+    def __eq__(self, other):
+        return self.amount == other.amount and self.currency == other.currency
 
     def times(self, multiplier):
-        return Dollar(self.amount * multiplier)
+        return Money(self.amount * multiplier, self.currency)
+
+    def divide(self, divisor):
+        return Money(self.amount/divisor, self.currency)
 
 if __name__ == '__main__':
     unittest.main()
